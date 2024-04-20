00edb34034b570594adb6f452079d1d2aed9bb11
melch-raiol <melchraiol@hotmail.com>
2024-04-20 18:55:13 -0300

    update dashboard page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,6 +4,8 @@ import Head from "next/head";
 
 import { getSession } from 'next-auth/react';
 import { Textarea } from '../../components/textarea';
+import { FiShare } from 'react-icons/fi';
+import { FaTrash } from 'react-icons/fa';
 
 export default function Dashboard(){
     return(
@@ -31,6 +33,32 @@ export default function Dashboard(){
                         <button className={styles.button} type='submit'>Registrar</button>
                     </form>
                 </div>
+            </section>
+
+            <section className={styles.taskContainer}>
+                <h1>Minhas tarefas</h1>
+
+                <article className={styles.task}>
+                    <div className={styles.tagContainer}>
+                        <label className={styles.tag}>PUBLICO</label>
+                        <button className={styles.shareButton}>
+                            <FiShare
+                              size={22}
+                              color="#3183ff"
+                            />
+                        </button>
+                    </div>
+
+                    <div className={styles.taskContent}>
+                        <p>Minha primeira tarefa como exemplo</p>
+                        <button className={styles.trashButton}>
+                            <FaTrash
+                              size={24}
+                              color="#ea3140"
+                            />
+                        </button>
+                    </div>
+                </article>
 
             </section>
            </main>
